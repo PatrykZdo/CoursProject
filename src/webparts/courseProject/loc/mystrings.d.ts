c35becdf9a7eb2193970ae173ec7b2b5dbb25b10
Drollpig51 <2patryk0@wp.pl>
2024-10-16 17:09:09 +0200

    first commit

---
+++ src/webparts/courseProject/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ICourseProjectWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'CourseProjectWebPartStrings' {
+  const strings: ICourseProjectWebPartStrings;
+  export = strings;
+}
